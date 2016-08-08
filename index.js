2de5b5bd3f6283fa5cd3af27045326041a8c3943
Shantanu Bhadoria <shantanu@cpan.org>
2016-08-08 15:19:34 +0800

    Fixed linting and other code quality issues

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function SituScale(address) {
   }
 }
 
-SituScale.prototype.searchScales = function(callback) {
+function startScanning() {
   noble.on('stateChange', function(state) {
     if (state === 'poweredOn') {
       noble.startScanning(['6e400001b5a3f393e0a9e50e24dcca9e'], false);
@@ -20,26 +20,30 @@ SituScale.prototype.searchScales = function(callback) {
       noble.stopScanning();
     }
   });
+}
+
+setNotification(characteristic, status) {
+  characteristic.notify(status, function(error) {
+    if(error)
+      console.error(error);
+  });
+}
+
+SituScale.prototype.searchScales = function(callback) {
+  startScanning();
   noble.on('discover', function(peripheral) {
     if(typeof callback == 'function')
       callback(peripheral);
   });
-}
+};
 
 SituScale.prototype.stopSearching = function(callback) {
   noble.stopScanning(callback);
-}
+};
 
 SituScale.prototype.getWeight = function(callback) {
   var situScale = this;
-  noble.on('stateChange', function(state) {
-    if (state === 'poweredOn') {
-      noble.startScanning(['6e400001b5a3f393e0a9e50e24dcca9e'], false);
-    } else {
-      console.log("State is " + state + " (not poweredOn). Switch on your bluetooth dongle/module.");
-      noble.stopScanning();
-    }
-  });
+  startScanning();
 
   noble.on('discover', function(peripheral) {
     if(peripheral.id === situScale.address || peripheral.address === situScale.address) {
@@ -55,6 +59,9 @@ SituScale.prototype.getWeight = function(callback) {
         console.error(error);
 
       peripheral.discoverServices(['6e400001b5a3f393e0a9e50e24dcca9e'], function(error, services) {
+        if(error)
+          console.error(error);
+
         var weightService = services[0];
         getWeight(weightService);
       });
@@ -63,39 +70,33 @@ SituScale.prototype.getWeight = function(callback) {
 
   function getWeight(weightService) {
     weightService.discoverCharacteristics(['6e400003b5a3f393e0a9e50e24dcca9e'], function(error, characteristics) {
+      if(error)
+        console.error(error);
+
       var weightNotifyCharacteristic = characteristics[0];
       situScale.notifyCharacteristic = weightNotifyCharacteristic;
         weightNotifyCharacteristic.on('read', function(data, isNotification) {
           callback(parseInt(data.toString(0)));
         });
 
-        weightNotifyCharacteristic.notify(true, function(error) {
-          if(error)
-            console.error(error);
-        });
+        setNotification(weightNotifyCharacteristic, true);
     });
   }
-}
+};
 
 SituScale.prototype.disconnect = function() {
   var situScale = this;
   situScale.peripheral.disconnect();
-}
+};
 
 SituScale.prototype.startNotifications = function() {
   var situScale = this;
-  situScale.notifyCharacteristic.notify(true, function(error) {
-    if(error)
-      console.error(error);
-  });
-}
+  setNotification(situScale.notifyCharacteristic, true);
+};
 
 SituScale.prototype.stopNotifications = function() {
   var situScale = this;
-  situScale.notifyCharacteristic.notify(false, function(error) {
-    if(error)
-      console.error(error);
-  });
-}
+  setNotification(situScale.notifyCharacteristic, false);
+};
 
 module.exports = SituScale;
