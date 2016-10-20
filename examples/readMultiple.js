9978b62d4360569cf80c841deced5c988d6e14f9
Shantanu Bhadoria <shantanu@cpan.org>
2016-10-20 17:51:54 +0800

    cleared old code. TODO: Add typed code

--- examples/readMultiple.js
+++
@@ -1,8 +0,0 @@
-// readWeight.js
-var SituScale = require('../index.js');
-
-var scale = new SituScale();
-
-scale.getAllWeights(function(weight, scale) {
-  console.log("Scale with address: " + scale.address + ' notified weight of ' + weight + ' gms.');
-});
