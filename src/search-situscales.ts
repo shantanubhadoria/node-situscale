b69ecdb03d36f338ab4ce6e2013194f612973c1f
Shantanu Bhadoria <shantanu@cpan.org>
2016-10-21 11:44:35 +0800

    Fixed ts file

--- src/search-situscales.ts
+++ src/search-situscales.ts
@@ -2,7 +2,7 @@ import { SituScale } from "./situ-scale";
 
 /* tslint:disable:no-console */
 SituScale.searchScales((scale) => {
-  console.log("Discovered SITU scale with address: ", scale);
+  console.log("Discovered SITU scale with address: ", scale.address);
   scale.callback = (weight) => {
     console.log(weight, "Received from scale with address", scale.address);
   };
